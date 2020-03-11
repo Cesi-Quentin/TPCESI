a6facbaabfb4ba2dfa352fd00c1da90f2120f28c
ASTA <quentin.asta@viacesi.fr>
2020-03-11 15:00:45 +0100

    database

--- backoffice/models.py
+++ backoffice/models.py
@@ -1,28 +1,22 @@
 from django.db import models
 
 
-class Personnes(models.Model):
-    nom = models.CharField(max_lenght=150)
-    prenom = models.CharField(max_lenght=150)
-    datenaissance = models.DateTimeField()
-    adresse = models.TextField()
-    tel = models.IntegerField()
-    photo = models.TextField() #blob
-
-
-class Intervenants(models.Model):
-    entreprise = models.CharField(max_length=200)
-
-
-class Eleves(models.Model):
-    classeid = models.IntegerField()
-
-
 class Promotion(models.Model):
     nom = models.CharField(max_length=50)
 
 
-class Events(models.Model):
+class Personne(models.Model):
+    nom = models.CharField(max_length=150)
+    prenom = models.CharField(max_length=150)
+    datenaissance = models.DateTimeField()
+    adresse = models.TextField()
+    tel = models.IntegerField()
+    photo = models.TextField() #blob
+    statut = models.CharField(max_length=50)
+    promotion = models.ForeignKey(Promotion, on_delete=models.CASCADE)
+
+
+class Event(models.Model):
     nom = models.CharField(max_length=100)
     desc = models.TextField()
     date = models.DateTimeField()
@@ -34,7 +28,3 @@ class Publication(models.Model):
     title = models.TextField()
     author = models.CharField(max_length=50)
     pub_date = models.DateTimeField()
-    object = models.Manager()
-
-    def __unicode__(self):
-        return self.title
