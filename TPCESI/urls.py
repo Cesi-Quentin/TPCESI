c0458d0c1f75facd95bb0377dc4e9dd7f01a9117
bpaumard <benjamin53200@gmail.com>
2020-03-11 16:19:47 +0100

    ajout url

--- TPCESI/urls.py
+++ TPCESI/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
+from backoffice import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name="index"),
+    path('photos/', views.photos, name="photos"),
+    path('profil/', views.profil, name="profil"),
+    path('promo/', views.promo, name="promo"),
+    path('trombi/', views.trombi, name="trombi"),
 ]
