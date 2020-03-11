63deed8d46cc2db1aafac897d4d270442e78dd45
bpaumard <benjamin53200@gmail.com>
2020-03-11 15:55:23 +0100

    ajout des vues

--- backoffice/admin.py
+++ backoffice/admin.py
@@ -2,9 +2,7 @@ from django.contrib import admin
 from backoffice.models import *
 
 # Register your models here.
-admin.site.register(Personnes)
-admin.site.register(Intervenants)
-admin.site.register(Eleves)
+admin.site.register(Personne)
 admin.site.register(Promotion)
-admin.site.register(Events)
+admin.site.register(Event)
 admin.site.register(Publication)
