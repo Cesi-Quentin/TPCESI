ed11f0f4b8ac1c826d486593b0ae21cfa643b690
bpaumard <benjamin53200@gmail.com>
2020-03-13 09:29:02 +0100

    login required

--- backoffice/views.py
+++ backoffice/views.py
@@ -2,30 +2,31 @@ from django.shortcuts import render
 from django.template import Context, loader
 from backoffice.models import Promotion, Event
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 
-# Create your views here.
+@login_required
 def index(request):
     return render(request, 'index.html')
 
-
+@login_required
 def evenements(request):
     event_list = Event.object.all()
     return render(request, 'evenements.html', {'event_list': event_list})
 
-
+@login_required
 def photos(request):
     return render(request, 'photos.html')
 
-
+@login_required
 def profil(request):
     return render(request, 'profil.html')
 
-
+@login_required
 def promo(request):
     promo_list = Promotion.object.all()
     return render(request, 'promotions.html', {'promo_list': promo_list})
 
-
+@login_required
 def trombi(request):
     return render(request, 'trombinoscope.html')
